e3e3c8d11be41c8df141974fa37dd8b5bd49c2e5
bambosan <bambosan888@gmail.com>
2026-03-14 18:01:36 +0700

    huge clouds improvement

--- src/include/lib/clouds.glsl
+++ src/include/lib/clouds.glsl
@@ -9,8 +9,9 @@
 // https://www.shadertoy.com/view/XslGRr
 
 #define CLOUD_HEIGHT 180.0
-#define CLOUD_THICKNESS 200.0
+#define CLOUD_THICKNESS 150.0
 #define CLOUD_VOLUME_SSHADOW_STEPS 4
+#define CLOUD_VOLUME_MAX_STEP_COUNTS 200
 #define CLOUD_VOLUME_STEP_SPACE 10.0
 
 //terrain
@@ -32,7 +33,7 @@ CloudSetup calcCloudSetup(float direction, float camAltitude) {
     CloudSetup setup;
     setup.tMin = 0.0;
     setup.tMax = 1e6;
-    setup.stepCounts = 200;
+    setup.stepCounts = CLOUD_VOLUME_MAX_STEP_COUNTS;
     setup.isValidCloud = true;
 
     float cloudMaxY = CLOUD_HEIGHT + CLOUD_THICKNESS;
@@ -74,19 +75,21 @@ float calcCumulusModel(vec3 pos) {
     base += valueNoise(basePos * 2.0) * 0.5;
     base += valueNoise(basePos * 4.0) * 0.25;
     base += valueNoise(basePos * 8.0) * 0.125;
-    base *= 0.53;
+    base = saturate(base * 0.533333 - 0.2);
+
+    float topFade = exp(-(1.0 - heightFraction) * 20.0);
+    base = linearstep(topFade, 1.0, base);
 
     float bottomFade = exp(-heightFraction * 25.0);
-    float topFade = exp(-(1.0 - heightFraction) * 20.0);
-    base = linearstep(bottomFade + topFade, 1.0, base - 0.17);
+    base = linearstep(bottomFade, 1.0, base);
 
     //worley sculpting, exclude buttom layer
-    float wsculpting = worley3d(pos * 0.08);
-    base = linearstep(wsculpting * heightFraction * 0.95, 1.0, base);
+    float wsculpting = worley3d(pos * 0.15);
+    base = linearstep(wsculpting * heightFraction * 0.75, 1.0, base);
 
     //perlin worley sculpting
     float pwsculpting = perlinWorley3d(pos * 0.2 + windDir.xxy * 0.5);
-    base = linearstep(pwsculpting * (heightFraction * 0.2 + 0.05), 1.0, base);
+    base = linearstep(pwsculpting * heightFraction * 0.25, 1.0, base);
     return base;
 }
 
@@ -109,8 +112,8 @@ float calcDirectScattering(vec3 samplePos, vec3 lightDir, float costh) {
     float a = 1.0; //shadow
 
     for (int j = 0; j < 4; j++) {
-        float forward = PhaseM(costh, 0.7 * g) * 0.5;
-        float backward = PhaseM(costh, -0.1 * g) * 0.5;
+        float forward = PhaseHG(costh, 0.7 * g) * 0.5;
+        float backward = PhaseHG(costh, -0.1 * g) * 0.5;
         lighting += b * (forward + backward) * exp(-shadow * stepSpace * a);
 
         a *= 0.25;
@@ -131,32 +134,30 @@ vec4 calcCloud(vec3 worldDir, vec3 lightDir, float worldDist, float dither, bool
 
     vec2 lighting = vec2_splat(0.0);
     float wdepth = 0.0; //weighted depth, used for atmosphere contribution
+    float tweight = 0.0;
     float transmittance = 1.0;
 
     if (isTerrain) setup.tMax = min(setup.tMax, worldDist);
 
-    setup.tMin += dither * CLOUD_VOLUME_STEP_SPACE;
-
     for (int i = 0; i < setup.stepCounts; i++) {
-        vec3 samplePos = rayOrigin + setup.tMin * rayDir;
+        vec3 samplePos = rayOrigin + rayDir * (setup.tMin + dither * CLOUD_VOLUME_STEP_SPACE);
         float heightFraction = saturate((samplePos.y - CLOUD_HEIGHT) / CLOUD_THICKNESS);
 
         float density = calcCumulusModel(samplePos);
 
         if (density > 0.0) {
-            float dscattering = calcDirectScattering(samplePos, lightDir, costh);
-
             //indirect scatter just use layer gradient
+            float dscattering = calcDirectScattering(samplePos, lightDir, costh);
             vec2 lum = vec2(dscattering, heightFraction) * density;
 
             float stepTransmittance = exp(-density * CLOUD_VOLUME_STEP_SPACE);
 
             //hillaire sctter integration
             vec2 scatterInt = (lum - lum * stepTransmittance) / max(density, EPSILON);
-
             lighting += transmittance * scatterInt;
 
-            wdepth += transmittance * setup.tMin * density * CLOUD_VOLUME_STEP_SPACE;
+            wdepth += transmittance * setup.tMin;
+            tweight += transmittance;
             transmittance *= stepTransmittance;
         }
 
@@ -164,6 +165,7 @@ vec4 calcCloud(vec3 worldDir, vec3 lightDir, float worldDist, float dither, bool
         if (setup.tMin > setup.tMax) break;
     }
 
+    wdepth /= tweight;
     return vec4(lighting, wdepth, transmittance);
 }
 
@@ -175,12 +177,10 @@ float calcCloudTransmittanceOnly(vec3 worldDir, float worldDist, float dither, b
 
     if (isTerrain) setup.tMax = min(setup.tMax, worldDist);
 
-    setup.tMin += dither * CLOUD_VOLUME_STEP_SPACE;
-
     float transmittance = 1.0;
 
     for (int i = 0; i < setup.stepCounts; i++) {
-        vec3 samplePos = rayOrigin + setup.tMin * rayDir;
+        vec3 samplePos = rayOrigin + rayDir * (setup.tMin + dither * CLOUD_VOLUME_STEP_SPACE);
         float density = calcCumulusModel(samplePos);
         if (density > 0.0) transmittance *= exp(-density * CLOUD_VOLUME_STEP_SPACE);
 
@@ -209,31 +209,27 @@ float calcCloudShadow(vec3 position, vec3 lightDir, float hardness, CloudSetup s
 }
 
 // just weird shape cirrus but i kinda like it
-// TODO: unroll this
 float cirrusFbm(vec2 pos) {
     float tdensity = 0.0;
     float amplitude = 1.0;
-    float totalamp = 0.0;
-
-    vec2 wind = vec2(0.0, Time.x * 0.1);
 
     pos.y *= 0.3;
     pos.x += sin(pos.y * 3.0) * 0.2;
+    vec2 wind = vec2(0.0, Time.x * 0.05);
 
     for (int i = 0; i < 4; i++) {
         float density = valueNoise(pos + wind) * amplitude;
         tdensity += density;
         pos *= 3.0;
         pos.y += density * 2.0;
-        totalamp += amplitude;
         amplitude *= 0.5;
     }
 
-    return saturate(tdensity / totalamp - 0.25);
+    return saturate(tdensity * 0.533333 - 0.25);
 }
 
 void applyCirrusClouds(inout vec3 outColor, vec3 worldDir, vec3 lightDir, vec3 absorbColor, bool isTerrain) {
-    vec2 cloudpos = worldDir.xz / worldDir.y * 5.0;
+    vec2 cloudpos = worldDir.xz / worldDir.y * 2.5;
     float base = isTerrain ? 0.0 : cirrusFbm(cloudpos);
 
     //distance fade
@@ -243,14 +239,12 @@ void applyCirrusClouds(inout vec3 outColor, vec3 worldDir, vec3 lightDir, vec3 a
     float cirrusHeight = CLOUD_HEIGHT + CLOUD_THICKNESS + 200.0;
     base *= smoothstep(0.0, 180.0, cirrusHeight + WorldOrigin.y);
 
-    float transmittance = exp(-base * 0.5);
+    float transmittance = exp(-base * 0.1);
 
-    //it's even has HG phase
     float costh = dot(worldDir, lightDir);
-    float forward = PhaseM(costh, 0.8);
-    float backward = PhaseM(costh, -0.3) * 0.5;
+    float forward = PhaseHG(costh, 0.8);
 
-    outColor = outColor * transmittance + absorbColor * (0.25 + forward + backward) * (1.0 - transmittance);
+    outColor = outColor * transmittance + absorbColor * (1.0 + forward) * (1.0 - transmittance);
 }
 
 #endif
